e4d600ebe284abe2f02e376176ea3e0a71d7e37d
Jainam Shah <49831712+shahJainam961@users.noreply.github.com>
2020-08-02 01:08:33 +0530

    Level 4 Salt and Hash

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
-const md5 = require('md5')
+// const md5 = require('md5')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
@@ -36,33 +38,36 @@ app.get("/register",function(req,res){
 
 app.post("/register",function(req,res){
 
-    const newUser = new User({
-        email: req.body.email,
-        password: md5(req.body.password)
+    bcrypt.hash(req.body.password,saltRounds,function(err,hash){
+        const newUser = new User({
+            email: req.body.email,
+            password: hash
+        })
+        newUser.save(function(err){
+            if(err){console.log(err);}
+            else{res.render("secrets")}
+        })
     })
-    newUser.save(function(err){
-        if(err){console.log(err);}
-        else{res.render("secrets")}
-    })
-
 
 })
 
 app.post("/login",function(req,res){
 
    const email = req.body.email
-   const password = md5(req.body.password)
+   const password = req.body.password
 
    User.findOne({email: email},function(err,foundUser){
        if(err){console.log(err);}
        else{
            if(foundUser){
-               if(foundUser.password === password){
-                   res.render("secrets")
-               }
-               else{
-                   console.log("Password Incorrect");
-               }
+                bcrypt.compare(password,foundUser.password,function(err,result){
+                    if(result === true){
+                        res.render("secrets")
+                    }
+                    else{
+                        console.log("Incorrect Password");
+                    }
+                })
            }
            else{
                console.log("User not found");
