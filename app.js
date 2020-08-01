29f6c5287fb89f612718fc6cb40693928d7b751f
Jainam Shah <49831712+shahJainam961@users.noreply.github.com>
2020-08-02 00:31:27 +0530

    Level 3 Hash passwords

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
+const md5 = require('md5')
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-const encrypt = require('mongoose-encryption')
+// const encrypt = require('mongoose-encryption')
 const app = express()
 
 app.set('view engine', 'ejs');
@@ -16,7 +17,7 @@ const userSchema = new mongoose.Schema({
     password : String
 })
 
-userSchema.plugin(encrypt, { secret:process.env.KEY , encryptedFields:['password'] });
+// userSchema.plugin(encrypt, { secret:process.env.KEY , encryptedFields:['password'] });
 
 const User = new mongoose.model("User",userSchema)
 
@@ -37,7 +38,7 @@ app.post("/register",function(req,res){
 
     const newUser = new User({
         email: req.body.email,
-        password: req.body.password
+        password: md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){console.log(err);}
@@ -50,7 +51,7 @@ app.post("/register",function(req,res){
 app.post("/login",function(req,res){
 
    const email = req.body.email
-   const password = req.body.password
+   const password = md5(req.body.password)
 
    User.findOne({email: email},function(err,foundUser){
        if(err){console.log(err);}
